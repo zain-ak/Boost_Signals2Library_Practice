d01ceb50d174547678e4a0465fc80e599372f10d
Zain Khalid <zak565@uowmail.edu.au>
2017-06-21 06:23:04 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Boost_ProgramOptionsLibrary_Practice
+Basic Practice Repo for Signals2 library from Boost for C++
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <boost/signals2.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace boost::signals2;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    return 0;
+}
+
